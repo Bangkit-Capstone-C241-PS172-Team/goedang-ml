6f3ac89d4db18ac6215d908caedca731e8046802
Aditya <ahdithiya@gmail.com>
2024-06-19 10:35:00 +0700

    adding api

---
+++ main.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import pickle
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+WINDOW_SIZE = 10
+STEP_AHEAD = 7
+
+def load_model(name):
+    path=f"models/model_{name}.h5"
+    model = tf.keras.models.load_model(path)
+    scaler = pickle.load(open(f"./models/scaler_{name}.pkl", 'rb'))
+    return model, scaler
+
+model_cmbtt, scaler_cmbtt = load_model("cmbtt") 
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/forecast", methods=["POST"])
+def forecast():
+    try:
+        data = request.get_json()
+        harga = data["harga"]
+        dates = data["tanggal"]
+        
+        if len(harga) < WINDOW_SIZE:
+            return jsonify({"message": "Data tidak cukup untuk melakukan forecasting"})
+        
+        last_window = np.array(harga[-WINDOW_SIZE:]).reshape(1, WINDOW_SIZE,1)
+        forecast = []
+        input_data = last_window
+        
+        for _ in range(STEP_AHEAD):
+            prediction = model_cmbtt.predict(input_data)
+            forecast.append(prediction[0,0])
+            input_data = np.append(input_data[:,1:,:], prediction.reshape(1,1,1), axis=1)
+        
+        forecast =scaler_cmbtt.inverse_transform(np.array(forecast).reshape(-1,1))
+        next_dates = pd.date_range(start=dates[-1],periods=STEP_AHEAD)
+        
+        response = {
+            "forecast": forecast.tolist(),
+            "dates": next_dates.strftime("%Y-%m-%d").tolist()
+        }
+        return jsonify(response)
+    
+    except Exception as e:
+        return jsonify({"message": str(e)})
+            
+        
+
+if __name__ == '__main__':
+    app.run()
