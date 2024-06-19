9fcf224482304d45492a4f783789f6950d2f127a
Rexhark <105967519+Rexhark@users.noreply.github.com>
2024-06-19 19:37:43 +0800

    add Dockerfile and requirements.txt

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
-WINDOW_SIZE = 10
+WINDOW_SIZE = 30
 STEP_AHEAD = 7
 
 def load_model(name):
@@ -16,38 +15,45 @@ def load_model(name):
     scaler = pickle.load(open(f"./models/scaler_{name}.pkl", 'rb'))
     return model, scaler
 
-model_cmbtt, scaler_cmbtt = load_model("cmbtt") 
-
-
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
 
 @app.route("/forecast", methods=["POST"])
 def forecast():
     try:
         data = request.get_json()
+        
+        if 'item' not in data or 'harga' not in data or 'tanggal' not in data:
+            return jsonify({"message": "Data tidak lengkap"}), 400
+        
+        item = data["item"]
         harga = data["harga"]
         dates = data["tanggal"]
         
-        if len(harga) < WINDOW_SIZE:
+        if item is None or harga is None or dates is None:
+            return jsonify({"message": "Data tidak lengkap"})
+        
+        model, scaler = load_model(item)
+        
+        if len(harga) < WINDOW_SIZE or len(dates) < WINDOW_SIZE:
             return jsonify({"message": "Data tidak cukup untuk melakukan forecasting"})
         
-        last_window = np.array(harga[-WINDOW_SIZE:]).reshape(1, WINDOW_SIZE,1)
+        last_window = np.array(harga[-WINDOW_SIZE:]).reshape(1, WINDOW_SIZE, 1)
         forecast = []
         input_data = last_window
         
         for _ in range(STEP_AHEAD):
-            prediction = model_cmbtt.predict(input_data)
-            forecast.append(prediction[0,0])
+            prediction = model.predict(input_data)
+            forecast.append(prediction[0, 0])
             input_data = np.append(input_data[:,1:,:], prediction.reshape(1,1,1), axis=1)
         
-        forecast =scaler_cmbtt.inverse_transform(np.array(forecast).reshape(-1,1))
-        next_dates = pd.date_range(start=dates[-1],periods=STEP_AHEAD)
+        forecast = scaler.inverse_transform(np.array(forecast).reshape(-1, 1)).flatten().tolist()
+        first_date = pd.to_datetime(dates[-1]) + pd.DateOffset(days=1)
+        next_dates = pd.date_range(start=first_date, periods=STEP_AHEAD)
         
         response = {
-            "forecast": forecast.tolist(),
+            "forecast": forecast,
             "dates": next_dates.strftime("%Y-%m-%d").tolist()
         }
         return jsonify(response)
